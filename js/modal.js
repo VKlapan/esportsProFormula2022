25486a6a86dc1be7d2d7ef04c9c57f271426e954
VKlapan <vladimir.klapan@gmail.com>
2022-11-12 19:51:29 +0200

    add all sections draft

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,23 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
+const pig = document.querySelectorAll("img[src='/images/custom/enemy.png']");
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+pig.forEach((item) => {
+  const coords = item.getBoundingClientRect();
+  const finX = 608;
+  const finY = 385;
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
-})();
+  let x = coords.x + 12;
+  let y = coords.y + 12;
+
+  let stepX = (finX - x) / 1000;
+  let stepY = (finY - y) / 1000;
+
+  const int = setInterval(() => {
+    x = x > finX ? x : x + stepX;
+    y = y > finY ? y : y + stepY;
+    if (x >= finX && y >= finY) {
+      clearInterval(int);
+      console.log("clear");
+    }
+    item.style.transform = `translate3d(${x}px, ${y}px, 0px)`;
+  }, 10);
+});
